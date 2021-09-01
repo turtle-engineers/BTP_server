bef2e5b2579470210b54799b951c9823d05cf837
lianachoi <2ndsunmail@gmail.com>
2021-09-02 01:02:15 +0900

    first commit

---
+++ models/user.js
@@ -0,0 +1,35 @@
+module.exports = function(sequelize, DataTypes){
+  let user = sequelize.define("User", {
+      Name: {
+          filed: "name",
+          type: DataTypes.STRING(50),
+          unique: true,
+          allowNull: false
+      },
+      Prvider : {
+        filed: "prvider",
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      PrviderId : {
+        filed: "prviderId",
+        type: DataTypes.STRING(50),
+        allowNull: false
+      },
+      Password: {
+          field: "password",
+          type: DataTypes.STRING(30),
+          allowNull: true
+      },
+      UserScore: {
+          field: "userScore",
+          type: DataTypes.BIGINT(30),
+          defaultValue: 0
+      }
+  }, {
+      underscored: true,
+      freezeTableName: true,
+      tableName: "user"
+  });
+  return user;
+}
